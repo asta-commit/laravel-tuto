d0e427858dda9647188b41fe1b35d963e36d50be
Ndeye asta Diallo <ndeyeasta95@gmail.com>
2021-09-09 16:45:39 +0000

    dernier commit

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>About Us | hello city</title>
-
-       
-    </head>
-    <body >
+@extends('app')
+@section('content')
         <p>Built with &hearts;by Asta.</p>
 
         <p><a href="/">Revenir vers la page d'accueil</a></p>
-        <footer> 
-         <p>
-             &copy; Copyright {{date('Y')}} &middot;
-             </p>   
-    </footer>
-    </body>
-</html>
+      
+@endsection
