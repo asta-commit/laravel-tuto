8d39ca4ea7a02fb174adcb2787701bdf5b911fa4
Ndeye asta Diallo <ndeyeasta95@gmail.com>
2021-09-22 16:28:24 +0000

    dernier mis a jour

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'pages.home')->name('home');
+
+Route::view('/about-us', 'pages.about')->name('about');
 
 
-Route::get('/', function () {
-    return view('pages/home');
-})->name('home');
 
-Route::get('/about-us', function () {
-  return view('pages/about');
-})->name('about');
-
-Route::get('/help', function () {
-  return view('pages.help');
-});
